410a40048d75751f2898341e571fb19b96688acc
Nasim-Akhtar89 <nasim.akhtar@whiteroselab.com>
2022-09-03 19:16:56 +0530

    database connected

--- config/database.config.js
+++ config/database.config.js
@@ -1,31 +1,31 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 let dbConnectionVar;
 const dbOptions = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-
 async function connectToDatabase(app) {
-	try {
-		dbConnectionVar = await mongoose.connect(process.env.dbUri, dbOptions);
-		console.log('Database Connected...');
-		app.listen(process.env.PORT || process.env.DEV_PORT);
-		console.log('Server Listening on Port :' +(process.env.PORT || process.env.DEV_PORT));
-	} catch(err) {
-		console.log('Failed to connect database...');
-		console.log('Either worng dbUri in .env');
-		console.log('---------- OR ------------');
-		console.log('database service is unavailable (turned off)');
-		console.log(err);
-	}
+  console.log("dburi : ", process.env.DBURI.slice(0, -1).replace(/^["'](.+(?=["']$))["']$/, "$1"));
+
+  const uri = process.env.DBURI.slice(0, -1).replace(/^["'](.+(?=["']$))["']$/, "$1");
+  try {
+    dbConnectionVar = await mongoose.connect(uri, dbOptions);
+    console.log("Database Connected...");
+    app.listen(8000 || process.env.DEV_PORT);
+    console.log("Server Listening on Port :" + (process.env.PORT || process.env.DEV_PORT));
+  } catch (err) {
+    console.log("Failed to connect database...");
+    console.log("Either worng dbUri in .env");
+    console.log("---------- OR ------------");
+    console.log("database service is unavailable (turned off)");
+    console.log(err);
+  }
 }
 
 module.exports = {
-	dbConnectionVar,
-	connectToDatabase
+  dbConnectionVar,
+  connectToDatabase,
 };
