6af65bd8aeb107076154a810d407aea95715b9ab
Nasim-Akhtar89 <nasim.akhtar@whiteroselab.com>
2022-09-05 19:23:34 +0530

    port updated

--- config/database.config.js
+++ config/database.config.js
@@ -11,11 +11,12 @@ async function connectToDatabase(app) {
   console.log("dburi : ", process.env.DBURI.slice(0, -1).replace(/^["'](.+(?=["']$))["']$/, "$1"));
 
   const uri = process.env.DBURI.replace(";", "").replace(/^["'](.+(?=["']$))["']$/, "$1");
+  const port = process.env.DEV_PORT.replace(";", "");
   try {
     dbConnectionVar = await mongoose.connect(uri, dbOptions);
     console.log("Database Connected...");
-    app.listen(8000 || process.env.DEV_PORT);
-    console.log("Server Listening on Port :" + (process.env.PORT || process.env.DEV_PORT));
+    app.listen(8000 || port);
+    console.log("Server Listening either on Port :" + port + "     " + 8000);
   } catch (err) {
     console.log("Failed to connect database...");
     console.log("Either worng dbUri in .env");
