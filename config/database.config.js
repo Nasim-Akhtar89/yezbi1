63deefd9c2cdeda3410d9286e30d8057bde39929
Nasim-Akhtar89 <nasim.akhtar@whiteroselab.com>
2022-10-20 12:43:32 +0530

    Database uri updated

--- config/database.config.js
+++ config/database.config.js
@@ -8,9 +8,9 @@ const dbOptions = {
 };
 
 async function connectToDatabase(app) {
-  console.log("dburi : ", process.env.DBURI.slice(0, -1).replace(/^["'](.+(?=["']$))["']$/, "$1"));
+  console.log("DATABASE_URL : ", process.env.DATABASE_URL.slice(0, -1).replace(/^["'](.+(?=["']$))["']$/, "$1"));
 
-  const uri = process.env.DBURI.replace(";", "").replace(/^["'](.+(?=["']$))["']$/, "$1");
+  const uri = process.env.DATABASE_URL.replace(";", "").replace(/^["'](.+(?=["']$))["']$/, "$1");
   const port = process.env.DEV_PORT.replace(";", "");
   const PORT = port ? port : 8000;
   try {
@@ -20,7 +20,7 @@ async function connectToDatabase(app) {
     console.log("Server Listening either on Port :", PORT);
   } catch (err) {
     console.log("Failed to connect database...");
-    console.log("Either worng dbUri in .env");
+    console.log("Either worng DATABASE_URL in .env");
     console.log("---------- OR ------------");
     console.log("database service is unavailable (turned off)");
     console.log(err);
